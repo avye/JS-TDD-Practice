802d954e7f2065d43f007aa93488b23455361523
Andrew Vye <a.vye26@gmail.com>
2016-09-23 16:32:30 -0700

    (feat) Add in initial setup for ES6

---
+++ ES6/index.js
@@ -0,0 +1,1 @@
+export const testExample = false;
--- ES6/spec/spec.js
+++ ES6/spec/spec.js
@@ -1,2 +1,9 @@
 import { expect } from 'chai';
 import { sinon } from 'sinon';
+import * as code from '../index.js'
+
+describe('Test Suite', function() {
+  it('should have a failing test', function() {
+    expect(code.testExample).to.equal(true)
+  })
+});
