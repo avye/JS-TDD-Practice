fa8a37702428f810fb8268d3d027c0c72da9a78f
Andrew Vye <a.vye26@gmail.com>
2016-09-23 16:37:18 -0700

    (feat) Set up grunt to run mocha test for both ES5 and ES6

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,31 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // ********** FOR ES5 **********
+    // mochaTest: {
+    //   test: {
+    //     src: ['ES5/spec/**/*.js']
+    //   }
+    // }
+
+    // ********** FOR ES6 **********
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          require: 'babel-register'
+        },
+        src: ['ES6/**/*.js']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('test', [
+    'mochaTest'
+  ]);
 }
